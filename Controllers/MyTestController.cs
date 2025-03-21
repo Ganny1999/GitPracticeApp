65dae55fb6d46e5685321fce22208b3b390d9414
ganesh <ganeshnalegave1999@gmail.com>
2025-03-21 14:39:50 +0530

    Add project files.

---
+++ Controllers/MyTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitPracticeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTestController : ControllerBase
+    {
+        [HttpGet]
+        public string Display()
+        {
+            return "from local";
+        }
+    }
+}
